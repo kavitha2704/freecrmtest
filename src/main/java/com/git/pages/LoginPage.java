c772efb81f044ab1f63e16e2171e68f831b16ec0
kavitha <kavitha2029@gmail.com>
2020-11-03 18:24:38 +0530

    first commit

---
+++ src/main/java/com/git/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package com.git.pages;
+
+public class LoginPage
+{
+ public void loginpage()
+ {
+	 System.out.println("This is login page");
+ }
+}
---
+++ src/test/java/com/git/test/LoginTest.java
@@ -0,0 +1,6 @@
+package com.git.test;
+
+public class LoginTest
+{
+
+}
